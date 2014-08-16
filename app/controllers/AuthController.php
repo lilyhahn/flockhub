1f7af5a257337722720eaecea21693d57e890fef
Tobi Hahn <rainshapes@gmail.com>
2014-08-16 18:30:15 -0400

    auth

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -4,7 +4,9 @@ class AuthController extends BaseController{
 
 	public function login(){
 		$connection = new TwitterOAuth("T5VEKYSzlobklcmLi32IJbzZK", "XFJWx6H5skSyobTNn5myeR9S3t5SDLn31QPuXQEWJWv4SfaSy7");
-		$temporary_credentials = $connection->getRequestToken(OAUTH_CALLBACK);
+		$temporary_credentials = $connection->getRequestToken("http://octogenarian.tk/initial-commit/public/login");
+		$redirect_url = $connection->getAuthorizeURL($temporary_credentials); // Use Sign in with Twitter
+		echo $redirect_url;
 	}
 
 }

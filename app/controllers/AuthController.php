5553cb9e3aeb6e4455f463237329ccc33cf018eb
noahdaboss123 <nootboy2008@gmail.com>
2014-08-18 22:33:26 -0400

    login screen

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,11 +1,15 @@
 <?php
-include(app_path().'/../vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php');
+include(app_path().'\..\vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php');
 
 session_start(); // bad, but using until db is going
 
 class AuthController extends BaseController{
 
-	public function login(){
+	public function login()
+	{
+		return View::make('Auth.login');
+	}
+	public function handleLogin(){
 		$connection = new TwitterOAuth("T5VEKYSzlobklcmLi32IJbzZK", "XFJWx6H5skSyobTNn5myeR9S3t5SDLn31QPuXQEWJWv4SfaSy7");
 		$temporary_credentials = $connection->getRequestToken("http://octogenarian.tk/initial-commit/public/callback");
 		$redirect_url = $connection->getAuthorizeURL($temporary_credentials); // Use Sign in with Twitter

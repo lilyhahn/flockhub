f74676b56fdbf1bc9381755264faa72cbe00f4ba
Tobi Hahn <rainshapes@gmail.com>
2014-08-31 08:50:54 -0400

    working?!

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -14,15 +14,17 @@ class AuthController extends BaseController{
 		$temporary_credentials = $connection->getRequestToken("http://octogenarian.tk/initial-commit/public/callback");
 		$_SESSION['temp_credentials'] = $temporary_credentials;
 		$redirect_url = $connection->getAuthorizeURL($temporary_credentials); // Use Sign in with Twitter
-		echo $redirect_url;
+		header('Location: '. $redirect_url);
+		die();
 	}
 	public function callback(){
 		$connection = new TwitterOAuth("***REMOVED***", "***REMOVED***", $_SESSION['temp_credentials']['oauth_token'], $_SESSION['temp_credentials']['oauth_token_secret']);
 		$token_credentials = $connection->getAccessToken($_GET['oauth_verifier']);
 		$connection = new TwitterOAuth("***REMOVED***", "***REMOVED***", $token_credentials['oauth_token'],
 			$token_credentials['oauth_token_secret']);
+		$connection->host = "https://api.twitter.com/1.1/";
 		$status = $connection->post('statuses/update', array('status' => 'testing 1 2 3 is this thing on - @rainshapes'));
-		echo $status;
+		var_dump($status);
 	}
 
 }

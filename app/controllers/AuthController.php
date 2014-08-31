9f3f6f2c9b81cf828bdd7704af10ef5d4db41696
noahdaboss123 <nootboy2008@gmail.com>
2014-08-31 10:15:44 -0400

    storing tokesn in db

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -14,17 +14,25 @@ class AuthController extends BaseController{
 		$temporary_credentials = $connection->getRequestToken("http://octogenarian.tk/initial-commit/public/callback");
 		$_SESSION['temp_credentials'] = $temporary_credentials;
 		$redirect_url = $connection->getAuthorizeURL($temporary_credentials); // Use Sign in with Twitter
-		header('Location: '. $redirect_url);
-		die();
+		return Redirect::to($redirect_url);
 	}
 	public function callback(){
 		$connection = new TwitterOAuth("T5VEKYSzlobklcmLi32IJbzZK", "XFJWx6H5skSyobTNn5myeR9S3t5SDLn31QPuXQEWJWv4SfaSy7", $_SESSION['temp_credentials']['oauth_token'], $_SESSION['temp_credentials']['oauth_token_secret']);
 		$token_credentials = $connection->getAccessToken($_GET['oauth_verifier']);
 		$connection = new TwitterOAuth("T5VEKYSzlobklcmLi32IJbzZK", "XFJWx6H5skSyobTNn5myeR9S3t5SDLn31QPuXQEWJWv4SfaSy7", $token_credentials['oauth_token'],
-			$token_credentials['oauth_token_secret']);
+		$token_credentials['oauth_token_secret']);
+		if(is_null(User::where('oauth_token', '=', $token_credentials['oauth_token'])->first())){
+			$user = new User;
+			$user->oauth_token = $token_credentials['oauth_token'];
+			$user->oauth_token_secret = $token_credentials['oauth_token_secret'];
+			$user->save();
+		}
 		$connection->host = "https://api.twitter.com/1.1/";
 		$status = $connection->post('statuses/update', array('status' => 'testing 1 2 3 is this thing on - @rainshapes'));
-		var_dump($status);
+		dd($status);
 	}
 
 }
+
+
+

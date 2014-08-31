5e6d7beb8325524d6191dd1a1fd6ff5191593e6e
Tobi Hahn <rainshapes@gmail.com>
2014-08-31 07:45:31 -0400

    fix paths

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,5 +1,5 @@
 <?php
-include(app_path().'\..\vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php');
+include(app_path().'/../vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php');
 
 session_start(); // bad, but using until db is going
 

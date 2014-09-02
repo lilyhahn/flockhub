c2a2ecfd82eb33a5ca3a6145b35a6b5995654387
Noah Gansallo <nootboy2008@gmail.com>
2014-09-02 09:07:49 -0400

    timesatmps

---
+++ app/database/migrations/2014_09_02_130708_timestamps.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Timestamps extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function($table){
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}

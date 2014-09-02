4261f6b86ef95ac717e2ac3de0aa64a75b096df1
Tobi Hahn <rainshapes@gmail.com>
2014-09-02 10:24:29 -0400

    removed auth keys

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,5 +1,6 @@
 <?php
 include(app_path().'/../vendor/abraham/twitteroauth/twitteroauth/twitteroauth.php');
+require_once(app_path() . '/config/oauth.php');
 
 session_start(); // bad, but using until db is going
 
@@ -10,16 +11,16 @@ class AuthController extends BaseController{
 		return View::make('Auth.login');
 	}
 	public function handleLogin(){
-		$connection = new TwitterOAuth("***REMOVED***", "***REMOVED***");
-		$temporary_credentials = $connection->getRequestToken("http://octogenarian.tk/initial-commit/public/callback");
+		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
+		$temporary_credentials = $connection->getRequestToken(OAUTH_CALLBACK);
 		$_SESSION['temp_credentials'] = $temporary_credentials;
 		$redirect_url = $connection->getAuthorizeURL($temporary_credentials); // Use Sign in with Twitter
 		return Redirect::to($redirect_url);
 	}
 	public function callback(){
-		$connection = new TwitterOAuth("***REMOVED***", "***REMOVED***", $_SESSION['temp_credentials']['oauth_token'], $_SESSION['temp_credentials']['oauth_token_secret']);
+		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $_SESSION['temp_credentials']['oauth_token'], $_SESSION['temp_credentials']['oauth_token_secret']);
 		$token_credentials = $connection->getAccessToken($_GET['oauth_verifier']);
-		$connection = new TwitterOAuth("***REMOVED***", "***REMOVED***", $token_credentials['oauth_token'],
+		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $token_credentials['oauth_token'],
 		$token_credentials['oauth_token_secret']);
 		$isset_user = TwitterUser::where('oauth_token', '=', $token_credentials['oauth_token'])->first();
 		if(is_null($isset_user)){

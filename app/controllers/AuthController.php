680b5b18956c51bb3b328e57a03a0ccd9357c5d4
Tobi Hahn <rainshapes@gmail.com>
2014-09-02 09:29:53 -0400

    newlines

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -30,11 +30,11 @@ class AuthController extends BaseController{
 		}
 		$connection->host = "https://api.twitter.com/1.1/";
 		$account_info = $connection->get('account/verify_credentials');
-		echo("followers:" . $account_info->followers_count);
-		echo("following:" . $account_info->friends_count);
-		echo("name:" . $account_info->name);
-		echo("id:" . $account_info->id);
-		echo("screen name:" . $account_info->screen_name);
+		echo("followers:" . $account_info->followers_count . "\n");
+		echo("following:" . $account_info->friends_count . "\n");
+		echo("name:" . $account_info->name . "\n");
+		echo("id:" . $account_info->id . "\n");
+		echo("screen name:" . $account_info->screen_name . "\n");
 		die();
 		return Redirect::action('DashboardController@index');
 	}

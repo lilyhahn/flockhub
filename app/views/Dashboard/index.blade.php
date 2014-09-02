ab1521cbb45223c110f8057038154b1be2a08784
noahdaboss123 <nootboy2008@gmail.com>
2014-09-02 08:29:37 -0400

    fixed model

--- app/views/Dashboard/index.blade.php
+++ app/views/Dashboard/index.blade.php
@@ -1,19 +1,25 @@
 @extends('template')
+@section('title')
+Dashboard
+@stop
 @section('content')
-<div class="small-box bg-yellow">
-    <div class="inner">
-        <h3>
-            44
-        </h3>
-        <p>
-            User Registrations
-        </p>
+<div class="col-lg-3 col-xs-6">
+    <!-- small box -->
+    <div class="small-box bg-green">
+        <div class="inner">
+            <h3>
+                44
+            </h3>
+            <p>
+                New Followers
+            </p>
+        </div>
+        <div class="icon">
+            <i class="ion ion-person-add"></i>
+        </div>
+        <a href="#" class="small-box-footer">
+            More info <i class="fa fa-arrow-circle-right"></i>
+        </a>
     </div>
-    <div class="icon">
-        <i class="ion ion-person-add"></i>
-    </div>
-    <a href="#" class="small-box-footer">
-        More info <i class="fa fa-arrow-circle-right"></i>
-    </a>
 </div>
 @stop

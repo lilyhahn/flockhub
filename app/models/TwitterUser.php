376a3d586fd82b53df1f6f822416523d2134cef8
noahdaboss123 <nootboy2008@gmail.com>
2014-09-02 22:35:22 -0400

    redo

--- app/models/TwitterUser.php
+++ app/models/TwitterUser.php
@@ -4,11 +4,8 @@
 class TwitterUser extends Eloquent{
 	protected $table = 'users';
 
-<<<<<<< HEAD
-=======
 	public function analyzeFollower()
 	{
 		$this->hasOne('AnalyzeFollower');
 	}
->>>>>>> f39e9063c3d2fd7be4ba57d0982289f56d20cb6e
 }
